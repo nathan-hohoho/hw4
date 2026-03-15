711871a2a39990a4ea47b0594dd0eab94aa4a3f5
nathan-hohoho <nathan.ho@kellogg.northwestern.edu>
2026-03-15 22:41:16 +0000

    updated global layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+  # This makes sure the method runs before every single page load
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    # Use find_by so it returns nil (instead of crashing) if the ID is wrong
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
 end
