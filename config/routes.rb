db2a005ed296ed439da00bdb437a72707dc40438
nathan-hohoho <nathan.ho@kellogg.northwestern.edu>
2026-03-15 22:19:06 +0000

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+
+  get "/login", :controller => "sessions", :action => "new"
+  post "/login", :controller => "sessions", :action => "create"
+  get "/logout", :controller => "sessions", :action => "destroy"
+  resources "users"
   resources "entries"
   resources "places"
-  resources "sessions"
-  resources "users"
 end
