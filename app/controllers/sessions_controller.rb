d53b08c70f4c874c92ed8dec9080cea4876907cd
nathan-hohoho <nathan.ho@kellogg.northwestern.edu>
2026-03-15 22:10:01 +0000

    add user controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user && @user.authenticate(params["password"])
+      # 3. If successful, "log them in" by saving their ID in the session
+      session["user_id"] = @user.id
+      flash["notice"] = "Welcome, #{@user.username}!"
+      redirect_to "/places"
+    else
+      # 4. If unsuccessful, send them back to the login page
+      flash["notice"] = "Invalid email or password."
+      redirect_to "/login"
   end
 
   def destroy
